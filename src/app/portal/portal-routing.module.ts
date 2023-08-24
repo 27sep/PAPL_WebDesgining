6b2146610ecf0d512ad8c74a919127e70f12d424
SOUMYARANJAN SAHOO <96434925+27sep@users.noreply.github.com>
2023-08-24 18:21:34 +0530

    4 pages created
    all are push

--- src/app/portal/portal-routing.module.ts
+++ src/app/portal/portal-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeOwnerMasterComponent } from './home-owner-master/home-owner-master.component';
+import{AgreementTypeMasterComponent} from './agreement-type-master/agreement-type-master.component';
+import{AgreementMasterComponent} from './agreement-master/agreement-master.component';
+import { UnitRegistrationComponent } from './unit-registration/unit-registration.component';
+import { HomeRegistrationComponent } from './home-registration/home-registration.component';
 
 const routes: Routes = [
   {
     path: 'portal/home-owner-master',
     component: HomeOwnerMasterComponent
-  }
+  },
+  {
+    path: 'portal/agreement-type-master',
+    component: AgreementTypeMasterComponent
+  },
+  {
+    path: 'portal/unit-registration',
+    component: UnitRegistrationComponent
+  },
+  {
+    path: 'portal/home-registration',
+    component: HomeRegistrationComponent
+  },
+  {
+    path: 'portal/agreement-master',
+    component: AgreementMasterComponent
+  },
 ];
 
 @NgModule({
